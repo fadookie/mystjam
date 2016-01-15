a3350a2bd901af60d14296f23a485303eaf0f7fc
Eliot <fadookie@gmail.com>
2016-01-15 11:22:07 -0800

    Implement link panel button, stub controller and uru link sound effect teeeheeheee

---
+++ Assets/code/component/LinkController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LinkTo(string linkName) {
+		Debug.LogFormat("LinkController: Link to {0}", linkName);
+		SoundManager.instance.playOneShot(SoundManager.Sound.Link);
+	}
+}
---
+++ Assets/code/component/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioClip linkSound;
+	public AudioSource source;
+
+	public enum Sound {
+		Link,
+	}
+
+	public static SoundManager instance { get; set; }
+
+	void Awake() {
+		instance = this;
+	}
+
+	public void playOneShot(Sound sound) {
+		source.PlayOneShot(getClip(sound));
+	}
+
+	AudioClip getClip(Sound sound) {
+		switch(sound) {
+			case Sound.Link:
+				return linkSound;
+			default:
+				return null;
+		}
+	}
+}
