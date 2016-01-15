7ce511f81bd7f7ac5d400a0f44853c86c50be9c1
Eliot <fadookie@gmail.com>
2016-01-15 11:56:20 -0800

    Link full-screen fade through black

--- Assets/code/component/LinkController.cs
+++ Assets/code/component/LinkController.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LinkController : MonoBehaviour {
 
 	public GameObject linkPanelCamera;
+	public Image screenOverlay;
 
 	// Use this for initialization
 	void Awake () {
@@ -18,9 +20,30 @@ public class LinkController : MonoBehaviour {
 	IEnumerator linkRoutine(string linkName) {
 		Services.instance.Get<SoundManager>().playOneShot(SoundManager.Sound.Link);
 
-		yield return new WaitForSeconds(0.5f);
+		float routineStartTime = Time.time;
+		const float fadeDurationS = 0.5f;
+		float routineElapsedTime = 0;
+		float lerpPct = 0;
+
+		while (routineElapsedTime < fadeDurationS)
+		{
+			lerpPct =  routineElapsedTime / fadeDurationS;
+			Color overlayColor = new Color(0, 0, 0, Mathf.Lerp(0, 1, lerpPct));
+			screenOverlay.color = overlayColor;
+			yield return null;
+			routineElapsedTime += Time.deltaTime;
+		}
 
 		Camera.main.transform.position = linkPanelCamera.transform.position;
 		Camera.main.transform.rotation = linkPanelCamera.transform.rotation;
+
+		while (routineElapsedTime < fadeDurationS * 2)
+		{
+			lerpPct =  routineElapsedTime / (fadeDurationS * 2);
+			Color overlayColor = new Color(0, 0, 0, Mathf.Lerp(1, 0, lerpPct));
+			screenOverlay.color = overlayColor;
+			yield return null;
+			routineElapsedTime += Time.deltaTime;
+		}
 	}
 }
