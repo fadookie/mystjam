1779a54655c06b425dc1567a8d735a417f7d2ebe
Eliot <fadookie@gmail.com>
2016-01-15 11:35:52 -0800

    Start working on link coroutine

--- Assets/code/component/LinkController.cs
+++ Assets/code/component/LinkController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class LinkController : MonoBehaviour {
 
+	public GameObject linkPanelCamera;
+
 	// Use this for initialization
 	void Awake () {
 		Services.instance.Set<LinkController>(this);
@@ -10,6 +12,15 @@ public class LinkController : MonoBehaviour {
 	
 	public void LinkTo(string linkName) {
 		Debug.LogFormat("LinkController: Link to {0}", linkName);
+		StartCoroutine(linkRoutine(linkName));
+	}
+
+	IEnumerator linkRoutine(string linkName) {
 		Services.instance.Get<SoundManager>().playOneShot(SoundManager.Sound.Link);
+
+		yield return new WaitForSeconds(0.5f);
+
+		Camera.main.transform.position = linkPanelCamera.transform.position;
+		Camera.main.transform.rotation = linkPanelCamera.transform.rotation;
 	}
 }
