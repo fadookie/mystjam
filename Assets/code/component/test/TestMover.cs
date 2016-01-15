973298f5696ffd1bc50642e59976d6bc16b7cddd
Eliot <fadookie@gmail.com>
2016-01-15 10:03:49 -0800

    Experiment with baked GI lighting

---
+++ Assets/code/component/test/TestMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMover : MonoBehaviour {
+
+	public float moveAmplitude = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = transform.localPosition;
+		newPos.x = Mathf.Sin(Time.time) * moveAmplitude;
+		transform.localPosition = newPos;
+	}
+}
