02d82290aa899e0484d775545a0aa79d113e0b3e
Eliot <fadookie@gmail.com>
2016-01-16 17:07:31 -0800

    nicer link panel cam movement

---
+++ Assets/code/component/LinkPanelCameraMover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkPanelCameraMover : MonoBehaviour {
+	public float speed = 30.0f;
+	public Transform pivot;
+
+	// Use this for initialization
+	//	void Start () {
+	//	}
+
+	void Update () {
+		transform.RotateAround(pivot.position, Vector3.up, speed * Time.deltaTime);
+		transform.LookAt(pivot);
+	}
+
+}
